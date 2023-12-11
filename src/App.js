98f250b553bf8dd7a6d25f00861dd446b24a3046
emonKabir <emonkabir95@gmail.com>
2023-12-12 03:12:52 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState('');
+  const [otp, setOtp] = useState('');
+  const [flag, setFlag] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const handleSubmitNumber = (e) => {
+    setFlag(!flag);
+    setNumber('');
+  };
+
+  const handleSubmitOtp = (e) => {
+    //// make request here ////
+
+    setIsSubmitted(true);
+    //setFlag(!flag);
+    setOtp('');
+  };
+
+  const handleAnotherNumber = () => {
+    setIsSubmitted(false);
+    setFlag(false);
+  };
+
+  const renderNumber = () => (
+    <>
+      <input
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+        type="number"
+        placeholder="Enter your 11 digits number here. e.g- 019xxxxxxxx"
+      />
+      <button onClick={handleSubmitNumber} disabled={!number}>
+        Submit
+      </button>
+    </>
+  );
+
+  const renderOtp = () => (
+    <>
+      <input
+        disabled={isSubmitted}
+        value={otp}
+        onChange={(e) => setOtp(e.target.value)}
+        type="number"
+        placeholder="Enter OTP"
+      />
+      <button onClick={handleSubmitOtp} disabled={!otp}>
+        Submit
+      </button>
+      <button onClick={handleAnotherNumber} disabled={!isSubmitted}>
+        Add Another Number
+      </button>
+    </>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centered-container">
+      <div className="centered-form">
+        {!flag ? renderNumber() : renderOtp()}
+      </div>
     </div>
   );
 }
